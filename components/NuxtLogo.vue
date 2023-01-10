84894c79a061908be0e0ad40656d13545512da60
アレク <>
2023-01-10 06:27:43 +0000

    Initial commit

---
+++ components/NuxtLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>This is a component.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="flex flex-col items-center justify-center">
+    <p class="text-4xl text-zinc-500">Hello World!</p>
+    <p class="text-2xl">Testing something else</p>
+    <comp-one></comp-one>
+  </div>
+</template>
+
+<script>
+import CompOne from '@/components/NuxtLogo.vue'
+export default {
+  components: {
+    CompOne
+  } 
+}
+</script>
